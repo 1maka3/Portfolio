28527240f3f6823ba0ecbd9feae65f292d5be0bb
Guilherme - Maka <92003604+1maka3@users.noreply.github.com>
2024-03-16 17:46:46 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Color Flipper",
+    subtitle: "CSS | HTML | JavaScript",
+    description:
+      "A simple color flipper",
+    image: "./Fliper-De-Cor.png",
+    link: "https://github.com/1maka3/TreinosJS-FlipperDeCor",
+  },
+  {
+    title: "Roman numeral converter",
+    subtitle: "JavaScript | HTML",
+    description:
+      "A converter of Roman to Arabic and Arabic to Roman numerals",
+    image: "./Conversor-Romano.png",
+    link: "https://github.com/1maka3/Conversor-Numeros-Romanos",
+  },
+  {
+    title: "Account Divider of a Restaurant",
+    subtitle: "JavaScript | HTML | CSS",
+    description:
+      "A bill divider for a restaurant",
+    image: "./Divisor-De-Contas.png",
+    link: "https://github.com/1maka3/Divisor-de-contas",
+  },
+  {
+    title: "Game of life",
+    subtitle: "JavaScript | HTML | CSS",
+    description:
+      "A game of life created by John Horton Conway in 1970",
+    image: "./Jogo-Da-Vida.png",
+    link: "https://github.com/1maka3/Jogo-da-vida",
+  },
+  {
+    title: "Task list",
+    subtitle: "CSS | HTML | JavaScript",
+    description:
+      "A to-do list app with features like task priorities and due dates.",
+    image: "./Lista-Tarefa.png",
+    link: "https://github.com/1maka3/Lista-de-tarefas",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "HTML",
+  "CSS",
+  "GIT",
+  "Linux",
+];
